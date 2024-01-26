2e9bbfed8be161b15045a2e0b881f6a9794c8812
Mandar Kulkarni <mandar242@gmail.com>
2024-01-26 11:57:10 -0800

    minor fixes, remove gcloud/gsutil use

---
+++ tests/integration/targets/test_gcs_backend/files/force_delete_bucket.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+from google.cloud import storage
+
+def delete_bucket(bucket_name, project_name, credentials_file_path):
+    """Deletes a bucket. The bucket must be empty."""
+    storage_client = storage.Client(project=project_name)
+
+    bucket = storage_client.get_bucket(bucket_name)
+    bucket.delete(force=True)
+    print(f"Bucket {bucket.name} deleted")
+
+bucket_name = os.getenv("BUCKET_NAME")
+project_name = os.getenv("CLOUDSDK_CORE_PROJECT")
+credentials_file_path = os.getenv("GCP_SERVICE_ACCOUNT_CREDENTIALS_FILE")
+
+delete_bucket(bucket_name, project_name, credentials_file_path)
